80a2e66d9b026ed4f9950b2d9e047dee823b3a3f
Shennelk1409 <shennelk@nycstudents.net>
2025-09-16 16:27:36 +0000

    cooment

---
+++ Java-1-sample/Java-3-Activity/Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+  //Print your full name 
+  System.out.println(" K Shennel");
+
+  //Print your full name with your full address
+  
+  //Print your full name with your full address using only one print statement (challenge).
+  System.out.println("Joe Smith\n3443 corp kennedy St\nBayside\n11361");
+    
+
+  System.out.println("Hi");
+
+  }
+
+  
+}
--- Java-4-Discussion/Main.java
+++ Java-4-Discussion/Main.java
@@ -8,17 +8,17 @@ class Main {
   // comment
   void init(){
     // Number of cars
-       
+       int numOfCars = 20;
     // Investment amount in currency
-      
+      float investmtAmt = 200.50;
     // Person's first name
-      
+      String fName = "Mary";
     // Is a game over?
-      
+      boolean isGameOver = false;
     // Store the first initial of a name
-      
+      char fInitial = 'N';
     // Maximum number of customers in a theater (declare as a constant)
-      
+      final int MaxCust = 500;
 
   }
 
